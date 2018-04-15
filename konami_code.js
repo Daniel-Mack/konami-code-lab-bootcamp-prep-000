f7de2a002320706b0d15d54ed884f01ccf3049f8
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-15 14:00:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,17 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 function init() {
-function onKeyDownHandler(e) {
   let index = 0;
+  
+function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which)
  if (key === alphabet[index]) {
     index++;
+    
     if (index === alphabet.length) {
-      console.log(alert("Hurray!"));
-            index = 0;
+     alert("Hurray!")
+      }
     }
-  } else {
-    index = 0;
   }
 }
-}
+
+
+document.body.addeventListener()
