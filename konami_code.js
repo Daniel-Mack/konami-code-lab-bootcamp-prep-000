5cff92e8dab699fd59de56e048574d69d96a4044
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-15 15:24:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -29,7 +29,7 @@ let index = 0;
 function init() {
  
  document.body.addEventListener("keydown", function(e) {
-      const key = parseInt(e.detail || e.which) 
+      const key = parseInt(e.detail || e.location) 
  
   if (index === alphabet.length) {
      alert("Hurray!")
